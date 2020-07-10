580a7aa532dfa402500fd1db879f0166daf25556
celihome <46626576+celihome@users.noreply.github.com>
2020-07-10 14:47:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,21 @@ import torch.backends.cudnn as cudnn
 import numpy as np
 import PIL.Image as pil_image
 
-from models import ESPCN
+from models import CSRCNN
 from utils import convert_ycbcr_to_rgb, preprocess, calc_psnr
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights-file', type=str,default='BLAH_BLAH/outputs/x2/best.pth')
-    parser.add_argument('--image-file', type=str,default='person_res/Prid_2_espcn_x3.png')
-    parser.add_argument('--scale', type=int, default=2)#4的时候需要修改
+    parser.add_argument('--weights-file', type=str,default= )
+    parser.add_argument('--image-file', type=str,default= )
+    parser.add_argument('--scale', type=int, default= )
     args = parser.parse_args()
 
     cudnn.benchmark = True
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    model = ESPCN(scale_factor=args.scale).to(device)
+    model = CSRCNN(scale_factor=args.scale).to(device)
 
     state_dict = model.state_dict()
     for n, p in torch.load(args.weights_file, map_location=lambda storage, loc: storage).items():
@@ -55,4 +55,4 @@ if __name__ == '__main__':
     output = np.array([preds, ycbcr[..., 1], ycbcr[..., 2]]).transpose([1, 2, 0])
     output = np.clip(convert_ycbcr_to_rgb(output), 0.0, 255.0).astype(np.uint8)
     output = pil_image.fromarray(output)
-    output.save(args.image_file.replace('.', '_espcn_x{}.'.format(args.scale)))
+    output.save(args.image_file.replace('.', '_CSRCNN_x{}.'.format(args.scale)))
